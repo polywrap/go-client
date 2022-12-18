6f6dfebc11883455023b652c24618e134adb7b2b
n0cte <ilnur.a.g@gmail.com>
2022-12-19 00:20:47 +0300

    add decoder for structs

--- wasm/decode.go
+++ wasm/decode.go
@@ -7,66 +7,86 @@ import (
 	"github.com/consideritdone/polywrap-go/polywrap/msgpack"
 )
 
-func decode(decoder msgpack.Read, v *reflect.Value) error {
-	switch v.Kind() {
-	case reflect.Bool:
-		v.SetBool(decoder.ReadBool())
-	case reflect.Int8:
-		v.SetInt(int64(decoder.ReadI8()))
-	case reflect.Int16:
-		v.SetInt(int64(decoder.ReadI16()))
-	case reflect.Int32:
-		v.SetInt(int64(decoder.ReadI32()))
-	case reflect.Int64:
-		v.SetInt(int64(decoder.ReadI64()))
-	case reflect.Uint8:
-		v.SetUint(uint64(decoder.ReadU8()))
-	case reflect.Uint16:
-		v.SetUint(uint64(decoder.ReadU16()))
-	case reflect.Uint32:
-		v.SetUint(uint64(decoder.ReadU32()))
-	case reflect.Uint64:
-		v.SetUint(uint64(decoder.ReadU64()))
-	case reflect.Float32:
-		v.SetFloat(float64(decoder.ReadF32()))
-	case reflect.Float64:
-		v.SetFloat(float64(decoder.ReadF64()))
-	case reflect.String:
-		v.SetString(decoder.ReadString())
-	case reflect.Array, reflect.Slice:
-		aLn := int(decoder.ReadArrayLength())
-		if v.Kind() == reflect.Slice {
-			v.Set(reflect.MakeSlice(v.Type(), aLn, aLn))
-		}
-		for i := 0; i < aLn; i++ {
-			t := v.Index(i)
-			decode(decoder, &t)
-		}
-	case reflect.Map:
-		mLn := int(decoder.ReadMapLength())
-		v.Set(reflect.MakeMap(v.Type()))
-		for i := 0; i < mLn; i++ {
-			key := reflect.Indirect(reflect.New(v.Type().Key()))
-			value := reflect.Indirect(reflect.New(v.Type().Elem()))
-			decode(decoder, &key)
-			decode(decoder, &value)
-			v.SetMapIndex(key, value)
-		}
-	default:
-		return fmt.Errorf("unknown type: %s", v.Type())
-	}
-	return nil
-}
-
 func Decode[T any](data []byte) (T, error) {
 	var value *T = new(T)
 	context := msgpack.NewContext(fmt.Sprintf("decode value: %T", value))
 	decoder := msgpack.NewReadDecoder(context, data)
 	queue := []reflect.Value{reflect.Indirect(reflect.ValueOf(value))}
+	linker := make([][3]reflect.Value, 0)
 	for len(queue) > 0 {
 		v := queue[0]
 		queue = queue[1:]
-		decode(decoder, &v)
+		switch v.Kind() {
+		case reflect.Bool:
+			v.SetBool(decoder.ReadBool())
+		case reflect.Int8:
+			v.SetInt(int64(decoder.ReadI8()))
+		case reflect.Int16:
+			v.SetInt(int64(decoder.ReadI16()))
+		case reflect.Int32:
+			v.SetInt(int64(decoder.ReadI32()))
+		case reflect.Int64:
+			v.SetInt(int64(decoder.ReadI64()))
+		case reflect.Uint8:
+			v.SetUint(uint64(decoder.ReadU8()))
+		case reflect.Uint16:
+			v.SetUint(uint64(decoder.ReadU16()))
+		case reflect.Uint32:
+			v.SetUint(uint64(decoder.ReadU32()))
+		case reflect.Uint64:
+			v.SetUint(uint64(decoder.ReadU64()))
+		case reflect.Float32:
+			v.SetFloat(float64(decoder.ReadF32()))
+		case reflect.Float64:
+			v.SetFloat(float64(decoder.ReadF64()))
+		case reflect.String:
+			v.SetString(decoder.ReadString())
+		case reflect.Array, reflect.Slice:
+			aLn := int(decoder.ReadArrayLength())
+			if v.Kind() == reflect.Slice {
+				v.Set(reflect.MakeSlice(v.Type(), aLn, aLn))
+			}
+			for i := 0; i < aLn; i++ {
+				queue = append([]reflect.Value{v.Index(i)}, queue...)
+			}
+		case reflect.Map:
+			mLn := int(decoder.ReadMapLength())
+			if mLn > 0 {
+				v.Set(reflect.MakeMap(v.Type()))
+			}
+			for i := 0; i < mLn; i++ {
+				key := reflect.Indirect(reflect.New(v.Type().Key()))
+				value := reflect.Indirect(reflect.New(v.Type().Elem()))
+				queue = append([]reflect.Value{key, value}, queue...)
+				linker = append([][3]reflect.Value{{v, key, value}}, linker...)
+			}
+		case reflect.Struct:
+			t := v.Type()
+			if t.Name() == "Int" {
+				v.Set(reflect.ValueOf(decoder.ReadBigInt()))
+			} else {
+				sLn := int(decoder.ReadMapLength())
+				if sLn != v.NumField() {
+					return *value, fmt.Errorf("different number of fields")
+				}
+				for i := sLn - 1; i >= 0; i-- {
+					key := reflect.Indirect(reflect.New(reflect.TypeOf("")))
+					value := reflect.Indirect(reflect.New(t.Field(i).Type))
+					queue = append([]reflect.Value{key, value}, queue...)
+					linker = append([][3]reflect.Value{{v, key, value}}, linker...)
+				}
+			}
+		default:
+			return *value, fmt.Errorf("unknown type: %s", v.Type())
+		}
+	}
+	for i := range linker {
+		switch linker[i][0].Kind() {
+		case reflect.Map:
+			linker[i][0].SetMapIndex(linker[i][1], linker[i][2])
+		case reflect.Struct:
+			linker[i][0].FieldByName(linker[i][1].String()).Set(linker[i][2])
+		}
 	}
 	return *value, nil
 }
--- wasm/decode_test.go
+++ wasm/decode_test.go
@@ -1,6 +1,7 @@
 package wasm
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
@@ -22,6 +23,17 @@ func makeDecoderTest[T any](t *testing.T, name string, expected T) {
 }
 
 func TestDecode(t *testing.T) {
+	type simpleTestStruct struct {
+		One   int8
+		Two   float32
+		Three string
+	}
+	type testStruct struct {
+		SomeMap1 map[string]simpleTestStruct
+		SomeStr  string
+		SomeInt  int64
+		SomeMap2 map[string]simpleTestStruct
+	}
 	makeDecoderTest(t, "bool", true)
 	makeDecoderTest(t, "int8", int8(1))
 	makeDecoderTest(t, "int16", int16(1))
@@ -37,4 +49,25 @@ func TestDecode(t *testing.T) {
 	makeDecoderTest(t, "[4]int8", [4]int8{1, 2, 3, 4})
 	makeDecoderTest(t, "[]int8", []int8{1, 2, 3, 4})
 	makeDecoderTest(t, "map[string]int8", map[string]int8{"one": 1, "two": 2, "three": 3})
+	makeDecoderTest(t, fmt.Sprintf("%T", simpleTestStruct{}), simpleTestStruct{
+		One:   1,
+		Two:   2.2,
+		Three: "three",
+	})
+	makeDecoderTest(t, fmt.Sprintf("%T", testStruct{}), testStruct{
+		SomeMap1: map[string]simpleTestStruct{
+			"one": {
+				One:   1,
+				Two:   1.1,
+				Three: "one",
+			},
+			"two": {
+				One:   2,
+				Two:   2.2,
+				Three: "two",
+			},
+		},
+		SomeStr: "some root string",
+		SomeInt: 123123123123,
+	})
 }
