f05e80a7d2a909630f668caedacfe56528fd4283
n0cte <ilnur.a.g@gmail.com>
2022-12-21 12:50:39 +0300

    rename hof name and rename test structs

--- wasm/decode_test.go
+++ wasm/decode_test.go
@@ -8,7 +8,8 @@ import (
 	"github.com/consideritdone/polywrap-go/polywrap/msgpack/big"
 )
 
-func makeDecoderTest[T any](t *testing.T, name string, expected T) {
+// execDecoderTest
+func execDecoderTest[T any](t *testing.T, name string, expected T) {
 	t.Run(name, func(t *testing.T) {
 		data, err := Encode(expected)
 		if err != nil {
@@ -25,60 +26,62 @@ func makeDecoderTest[T any](t *testing.T, name string, expected T) {
 }
 
 func TestDecode(t *testing.T) {
-	type simpleTestStruct struct {
-		One   int8
-		Two   float32
-		Three string
-		Four  *string
-		Five  *big.Int
-	}
-	type testStruct struct {
-		SomeMap1 map[string]simpleTestStruct
-		SomeStr  string
-		SomeInt  int64
-		SomeMap2 map[string]simpleTestStruct
-		SomeMap3 map[string]*simpleTestStruct
-	}
+	type (
+		simpleStruct struct {
+			One   int8
+			Two   float32
+			Three string
+			Four  *string
+			Five  *big.Int
+		}
+		complexStruct struct {
+			SomeMap1 map[string]simpleStruct
+			SomeStr  string
+			SomeInt  int64
+			SomeMap2 map[string]simpleStruct
+			SomeMap3 map[string]*simpleStruct
+		}
+	)
 
 	trueValue := true
-	simpleTestStructValue := simpleTestStruct{
+	simpleStructValue := simpleStruct{
 		One:   1,
 		Two:   1.1,
 		Three: "one",
 	}
 
-	makeDecoderTest(t, "bool=true", trueValue)
-	makeDecoderTest(t, "*bool=true", &trueValue)
-	makeDecoderTest[*bool](t, "*bool=nil", nil)
-	makeDecoderTest(t, "int8", int8(1))
-	makeDecoderTest(t, "int16", int16(1))
-	makeDecoderTest(t, "int32", int32(1))
-	makeDecoderTest(t, "int64", int64(1))
-	makeDecoderTest(t, "uint8", uint8(1))
-	makeDecoderTest(t, "uint16", uint16(1))
-	makeDecoderTest(t, "uint32", uint32(1))
-	makeDecoderTest(t, "uint64", uint64(1))
-	makeDecoderTest(t, "float32", float32(1.12))
-	makeDecoderTest(t, "float64", float64(1.13))
-	makeDecoderTest(t, "string", "hello world")
-	makeDecoderTest(t, "[4]int8", [4]int8{1, 2, 3, 4})
-	makeDecoderTest(t, "[]int8", []int8{1, 2, 3, 4})
-	makeDecoderTest(t, "map[string]int8", map[string]int8{"one": 1, "two": 2, "three": 3})
-	makeDecoderTest(t, fmt.Sprintf("%T", simpleTestStruct{}), simpleTestStruct{
+	execDecoderTest(t, "bool=true", trueValue)
+	execDecoderTest(t, "*bool=true", &trueValue)
+	execDecoderTest[*bool](t, "*bool=nil", nil)
+	execDecoderTest(t, "int8", int8(1))
+	execDecoderTest(t, "int16", int16(1))
+	execDecoderTest(t, "int32", int32(1))
+	execDecoderTest(t, "int64", int64(1))
+	execDecoderTest(t, "uint8", uint8(1))
+	execDecoderTest(t, "uint16", uint16(1))
+	execDecoderTest(t, "uint32", uint32(1))
+	execDecoderTest(t, "uint64", uint64(1))
+	execDecoderTest(t, "float32", float32(1.12))
+	execDecoderTest(t, "float64", float64(1.13))
+	execDecoderTest(t, "string", "hello world")
+	execDecoderTest(t, "[4]int8", [4]int8{1, 2, 3, 4})
+	execDecoderTest(t, "[]int8", []int8{1, 2, 3, 4})
+	execDecoderTest(t, "map[string]int8", map[string]int8{"one": 1, "two": 2, "three": 3})
+	execDecoderTest(t, fmt.Sprintf("%T", simpleStruct{}), simpleStruct{
 		One:   1,
 		Two:   2.2,
 		Three: "three",
 		Five:  big.NewInt(1),
 	})
-	makeDecoderTest(t, fmt.Sprintf("%T", &simpleTestStruct{}), &simpleTestStruct{
+	execDecoderTest(t, fmt.Sprintf("%T", &simpleStruct{}), &simpleStruct{
 		One:   1,
 		Two:   2.2,
 		Three: "three",
 		Five:  big.NewInt(1),
 	})
-	makeDecoderTest[*simpleTestStruct](t, fmt.Sprintf("%T", &simpleTestStruct{}), nil)
-	makeDecoderTest(t, fmt.Sprintf("%T", testStruct{}), testStruct{
-		SomeMap1: map[string]simpleTestStruct{
+	execDecoderTest[*simpleStruct](t, fmt.Sprintf("%T", &simpleStruct{}), nil)
+	execDecoderTest(t, fmt.Sprintf("%T", complexStruct{}), complexStruct{
+		SomeMap1: map[string]simpleStruct{
 			"one": {
 				One:   1,
 				Two:   1.1,
@@ -94,8 +97,8 @@ func TestDecode(t *testing.T) {
 		},
 		SomeStr: "some root string",
 		SomeInt: 123123123123,
-		SomeMap3: map[string]*simpleTestStruct{
-			"one": &simpleTestStructValue,
+		SomeMap3: map[string]*simpleStruct{
+			"one": &simpleStructValue,
 			"two": nil,
 		},
 	})
