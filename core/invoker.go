2b7f2f3bd2627150efa8dce7b4be619fef2fc811
n0cte <ilnur.a.g@gmail.com>
2022-12-21 19:26:10 +0300

    split Invoker interface

--- core/invoker.go
+++ core/invoker.go
@@ -2,9 +2,20 @@ package core
 
 import "github.com/polywrap/go-client/core/resolver/uri"
 
-type Invoker interface {
-	Invoke(uri uri.URI, method string, args []byte, env []byte) ([]byte, error)
-	InvokeWrapper(wrapper any, uri uri.URI, method string, args []byte, env []byte) ([]byte, error)
-	Implementations(uri uri.URI) ([]uri.URI, error)
-	Interfaces() map[string][]uri.URI
-}
+type (
+	Invoker interface {
+		Invoke(uri uri.URI, method string, args []byte, env []byte) ([]byte, error)
+	}
+
+	WrapperInvoker interface {
+		InvokeWrapper(wrapper any, uri uri.URI, method string, args []byte, env []byte) ([]byte, error)
+	}
+
+	ImplementationInvoker interface {
+		Implementations(uri uri.URI) ([]uri.URI, error)
+	}
+
+	InterfaceInvoker interface {
+		Interfaces() map[string][]uri.URI
+	}
+)
